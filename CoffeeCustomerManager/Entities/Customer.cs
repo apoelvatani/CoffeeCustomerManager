2f4831a8fa462ec122741a79db35fc3a535b9acd
apoelvatani <76132938+Chappo588@users.noreply.github.com>
2022-07-02 13:56:15 +0300

    Add project files.

--- CoffeeCustomerManager/Entities/Customer.cs
+++ CoffeeCustomerManager/Entities/Customer.cs
@@ -11,6 +11,5 @@ namespace CoffeeCustomerManager.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int YearOfBirth { get; set; }
-
     }
 }
